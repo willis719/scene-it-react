11ddbda3cac50146a481c4e728dfe7a33843bf8f
Willie Rose <willierose87@gmail.com>
2021-04-05 15:36:02 -0500

    Added react router

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
